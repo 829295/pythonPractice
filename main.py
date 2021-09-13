0f5ea1a2872b164635d196e786d6f8f59e318370
829295 <829295@mystma.org>
2021-09-13 15:17:31 -0500

    created main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Hello World!")
+print("The FitnessGram Pacer Test is a multi-stage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start.")
+print("Goodbye World!")
