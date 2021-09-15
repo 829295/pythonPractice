c9072b0c45b052c659ff37cf7791ffb50308f222
829295 <829295@mystma.org>
2021-09-15 14:43:12 -0500

    added 2 more functions

--- main.py
+++ main.py
@@ -20,5 +20,47 @@ def topThreeMovies():
     # print third movie
     print("3. The Lighthouse")
 
+    time.sleep(6)
 
 topThreeMovies()
+
+print("Colin's Top Three Books:")
+
+def topThreeBooks():
+    time.sleep(3)
+
+    # print first book
+    print("1. To Kill a Mockingbird")
+
+    time.sleep(3)
+
+    #print second book
+    print("2. The Hunger Games")
+
+    time.sleep(3)
+
+    #print third book
+    print("3. Slaughterhouse Five")
+
+    time.sleep(3)
+
+topThreeBooks()
+
+print("Colin's Top Three Bands:")
+def topThreeBands():
+
+    time.sleep(3)
+
+    print("1. The Strokes")
+
+    time.sleep(3)
+
+    print("2. Sleep Year")
+
+    time.sleep(3)
+
+    print("3. Post-Modern Connection")
+
+    time.sleep(3)
+
+topThreeBands()
